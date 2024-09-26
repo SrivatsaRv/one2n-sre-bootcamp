c80c03a67e710fa809f1c9ef8dc0942749223004
“srivatsa.rv” <srivatsa.rv@one2n.in>
2024-09-26 15:29:11 +0530

    milestone-2/makefile-working , schemamigration-working, app-working, database-working

--- milestone-2-containerize-the-api/models.py
+++ milestone-2-containerize-the-api/models.py
@@ -6,7 +6,7 @@ class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    grade = db.Column(db.String(20), nullable=False)
+    #grade = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
         return f"<Student {self.name}>"
